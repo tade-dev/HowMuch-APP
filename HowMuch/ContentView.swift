c67727622f8a3f42fb8da20f2a5efd538301479c
Akintade Oluwaseun <akintadeseun816@gmail.com>
2026-01-07 16:14:35 +0000

    Initial Commit

---
+++ HowMuch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowMuch
+//
+//  Created by BSTAR on 07/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowMuch/HowMuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowMuchApp.swift
+//  HowMuch
+//
+//  Created by BSTAR on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct HowMuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
